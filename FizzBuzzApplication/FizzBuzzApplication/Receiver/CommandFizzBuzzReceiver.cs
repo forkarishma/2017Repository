a97c5ca79b82fdeab1801d185ac38263ccb0c1e4
Karishma <forkarishma@gmail.com>
2017-03-05 17:19:07 +0000

    implement base class CommandService

--- FizzBuzzApplication/FizzBuzzApplication/Receiver/CommandFizzBuzzReceiver.cs
+++ FizzBuzzApplication/FizzBuzzApplication/Receiver/CommandFizzBuzzReceiver.cs
@@ -1,9 +1,14 @@
 ﻿using FizzBuzzApplication.Interfaces;
+using FizzBuzzApplication.Services;
 
 namespace FizzBuzzApplication.Receiver
 {
     public class CommandFizzBuzzReceiver : ICommandFizzBuzzReceiver
     {
-         
+        //set all services available and sent it to invoker
+        public CommandService ReceiveFizzBuzzService()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- FizzBuzzApplication/FizzBuzzApplication/Services/CommandService.cs
+++ FizzBuzzApplication/FizzBuzzApplication/Services/CommandService.cs
@@ -2,8 +2,27 @@
 
 namespace FizzBuzzApplication.Services
 {
-    public class CommandService : ICommandService
+    public abstract class CommandService : ICommandService
     {
-         
+        public abstract bool CanServiceFizzBuzz(long number);
+        public abstract string ServiceFizzBuzz(long number);
+
+        protected CommandService Service;
+
+        //Provide Service and if not found check for another service
+        protected internal string ProvideServiceFizzBuzz(long number)
+        {
+            return CanServiceFizzBuzz(number) ? ServiceFizzBuzz(number) : Service.ProvideServiceFizzBuzz(number);
+        }
+
+        //set chain of services available
+        protected internal CommandService SetService(ICommandService newService)
+        {
+            if (Service == null)
+                Service = (CommandService)newService;
+            else
+                Service.SetService(newService);
+            return this;
+        }
     }
 }

7c8bee25706c0e8235fdb49fd12e993ecc39094d
Karishma <forkarishma@gmail.com>
2017-03-05 19:11:58 +0000

    create CommandBuzzService and CommandBuzzServiceTests class

---
+++ FizzBuzzApplication/FizzBuzzApplicationTests/Services/CommandBuzzServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzApplicationTests.Services
+{
+    [TestClass()]
+    public class CommandBuzzServiceTests
+    {
+        [TestMethod()]
+        public void CanServiceFizzBuzzTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ServiceFizzBuzzTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

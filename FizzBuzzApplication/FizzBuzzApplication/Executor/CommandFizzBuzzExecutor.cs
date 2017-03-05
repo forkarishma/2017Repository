6a62cc4b3f1c0f5cc3f5572a364ee9cf15ee14f0
Karishma <forkarishma@gmail.com>
2017-03-05 16:59:04 +0000

    Create CommandFizzBizzExecutor Object in  main, initialise receiver via CommandFizzBizzExecutor constructor

--- FizzBuzzApplication/FizzBuzzApplication/Executor/CommandFizzBuzzExecutor.cs
+++ FizzBuzzApplication/FizzBuzzApplication/Executor/CommandFizzBuzzExecutor.cs
@@ -4,6 +4,14 @@ namespace FizzBuzzApplication.Executor
 {
     public class CommandFizzBuzzExecutor : ICommandFizzBuzzExecutor
     {
+        private ICommandService _cmdFizzBuzzService;
+        private readonly ICommandFizzBuzzReceiver _cmdFizzBuzzReceiver;
+
+        internal CommandFizzBuzzExecutor(ICommandFizzBuzzReceiver cmdFizzBuzzReceiver)
+        {
+            this._cmdFizzBuzzReceiver = cmdFizzBuzzReceiver;
+        }
+
         public string ExecuteFizzBuzz(long number)
         {
             throw new System.NotImplementedException();
--- FizzBuzzApplication/FizzBuzzApplication/Program.cs
+++ FizzBuzzApplication/FizzBuzzApplication/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzBuzzApplication.Executor;
+using FizzBuzzApplication.Interfaces;
+using FizzBuzzApplication.Receiver;
 
 namespace FizzBuzzApplication
 {
@@ -14,6 +17,7 @@ namespace FizzBuzzApplication
             long number = Convert.ToInt64(Console.ReadLine());
             //implement fizzbuzz functionality
             //execute commandfizzbuzz
+            ICommandFizzBuzzExecutor cmdFizzBuzzExecutor = new CommandFizzBuzzExecutor(new CommandFizzBuzzReceiver());
         }
     }
 }

2bbafd8a36848e04c8f3c5d8688f19e27e55dbc7
Karishma <forkarishma@gmail.com>
2017-03-05 17:37:09 +0000

    CommandFizzBuzzServiceTests.CanServiceFizzBuzzTest_NumberNotFizzBuzz_ReturnTrue Test fail

--- FizzBuzzApplication/FizzBuzzApplicationTests/Services/CommandFizzBuzzServiceTests.cs
+++ FizzBuzzApplication/FizzBuzzApplicationTests/Services/CommandFizzBuzzServiceTests.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FizzBuzzApplication.Interfaces;
+using FizzBuzzApplication.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FizzBuzzApplicationTests.Services
 {
     [TestClass()]
     public class CommandFizzBuzzServiceTests
     {
+        private ICommandService cmdService;
+        private long number = 1234;
+
         [TestMethod()]
-        public void CanServiceFizzBuzzTest()
+        public void CanServiceFizzBuzzTest_NumberNotFizzBuzz_ReturnTrue()
         {
-            Assert.Fail();
+            //Arrange
+            cmdService = new CommandFizzBuzzService();
+            bool result;
+
+            //Act
+            result = cmdService.CanServiceFizzBuzz(number);
+
+            //Assert
+            Assert.IsTrue(result);
         }
     }
 }
